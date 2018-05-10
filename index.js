27465c3b69399fbe013b7e69d640a073e732791b
Jerry Wiltz <jerry.wiltz@gmail.com>
2018-05-10 11:57:37 -0700

    Add files via upload
    This is the first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export {axisLeft, axisBottom} from 'd3-axis';
+export {select}               from 'd3-selection';
+export {schemeCategory10}     from 'd3-scale-chromatic';
+export {line}                 from 'd3-shape';
+export {tsvParse}             from 'd3-dsv';
+export {scaleLinear}          from 'd3-scale';
+export {extent}               from 'd3-array'
